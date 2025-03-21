ed5d379d57334043db66b7f41d1ec89e630a2fa7
Tej749 <jargha261975@gmail.com>
2025-03-22 05:35:45 +0545

    domain

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:5174", "https://react-fin-project.vercel.app"],
+    origin: ["http://localhost:5174", "https://react-fin-project.vercel.app", "https://tezn749.xyz" ],
   })
 );
 
